0c9b1142a0cfac681c090ffb184a4d4754c18aff
nonameuser2019 <zhe.depotop@gmail.com>
2021-08-12 18:47:05 +0300

    Add test in by one click

--- tests/test_buy_one_click.py
+++ tests/test_buy_one_click.py
@@ -19,7 +19,6 @@ class TestOrderByOneClick:
                                                         'result: {result.json()["success"]}'
         check.equal(len(data), 1), f'Wrong product count in DB, expected result is 1 but actual result {len(data)}'
 
-    @pytest.mark.test
     @pytest.mark.smoke
     def test_send_request_with_product_with_attr_id(self, db_connect):
         body = JsonFixture.order_by_click(product_id="109189", attribute_id="1")
@@ -31,3 +30,13 @@ class TestOrderByOneClick:
         check.equal(result.json()['success'], True), f'Wrong success result. Expected result is True. Actual ' \
                                                      'result: {result.json()["success"]}'
         check.equal(len(data), 1), f'Wrong product count in DB, expected result is 1 but actual result {len(data)}'
+
+    @pytest.mark.test
+    @pytest.mark.smoke
+    def test_send_req_with_unavailable_product(self):
+        body = JsonFixture.order_by_click(product_id="249953")
+        result = HttpManager.post(OrdersByClick.oreder_by_click_endpoint, body, JsonFixture.get_header_without_token())
+        check.equal(result.status_code, 400), f'Wrong status code. Expected result is 200. Actual ' \
+                                              f'result is {result.status_code}'
+        check.equal(result.json()['success'], False), f'Wrong success result. Expected result is True. Actual ' \
+                                                     'result: {result.json()["success"]}'
